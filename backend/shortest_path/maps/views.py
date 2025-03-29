5f85dd3c51b258c3c757fdd6e3f5589e084ca654
Prakhar54-byte <162185166+Prakhar54-byte@users.noreply.github.com>
2025-03-30 04:48:03 +0530

    add function of haversine and find_closest_node

--- backend/shortest_path/maps/views.py
+++ backend/shortest_path/maps/views.py
@@ -1,3 +1,6 @@
+import math
+
+
 from django.shortcuts import render
 from django.core.cache import cache
 from .models import Node, Edge
@@ -18,3 +21,30 @@ def load_graph():
     
     cache.set("graph_data", graph)
     return graph
+
+
+def haversine(lat1, lon1, lat2, lon2):
+    R = 6371  # Radius of the Earth in km
+    lat1, lon1, lat2, lon2 = map(math.radians, [lat1, lon1, lat2, lon2])
+    
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    
+    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    
+    return R * c
+
+
+def find_closest_node(lat,lon):
+    nodes = Node.objects.all()
+    closest_node = None
+    min_distance = float('inf')
+    
+    for node in nodes:  
+        distance = haversine(lat, lon, node.latitude, node.longitude)
+        if distance < min_distance:
+            min_distance = distance
+            closest_node = node.id
+    return closest_node
+    
